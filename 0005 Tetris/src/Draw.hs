5ab6eb62d4d9a54e7ed124aa03200d426a3925ea
Tom Janssens <tom@corebvba.be>
2015-10-26 18:55:32 +0100

    A working tetris game

---
+++ 0005 Tetris/src/Draw.hs
@@ -0,0 +1,58 @@
+module Draw(myPlay) where
+
+import Game
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+
+cellSize :: Int
+cellSize = 40;
+
+drawBlock :: Position -> Block -> Picture
+drawBlock p b = Pictures $ map drawCell $ blockToCells p b
+
+drawCell :: Cell -> Picture
+drawCell (Cell blockcolor (Position xi yi)) =  Color (getColor blockcolor) poly
+  where
+    [x,y,cs] = map fromIntegral [xi*cellSize,yi*cellSize,cellSize-1]
+    poly = Polygon [(x,y), (x+cs,y), (x+cs,y+cs), (x,y+cs)]
+    getColor c = case c of
+      Red -> red
+      Green -> green
+      Blue -> blue
+      Yellow -> yellow
+      Cyan -> cyan
+      Orange -> orange
+      Magenta -> magenta
+
+drawLevel :: Level -> Picture
+drawLevel (Level board state _score) =
+  Pictures $ drawBoard board : drawState state
+  where
+    drawBoard (Board cells)= Pictures $ map drawCell cells
+    drawState (BlockFalling pos block) = [drawBlock pos block]
+    drawState _ = []
+
+
+
+myPlay :: Level -> (LevelEvent -> Level -> IO Level) -> (Float -> Level -> IO Level) -> IO ()
+myPlay wrld eventHandler =
+  playIO window black
+    3 -- simulations/sec
+    wrld
+    drawWrld
+    internalEventHandler
+  where
+    window = InWindow "HaskTris by @ToJans" (w, h) (100, 100)
+    w = cellSize * gridWidth
+    h = cellSize * gridHeight
+    drawWrld wrld = return
+                $ translate (-fromIntegral w/2) (-fromIntegral h/2)
+                $ drawLevel wrld
+    internalEventHandler:: Event -> Level -> IO Level
+    internalEventHandler evt = maybe return eventHandler $ maybeEvent evt
+    maybeEvent evt = case evt of
+        EventKey (SpecialKey KeyLeft) Down _ _  -> Just MoveLeft
+        EventKey (SpecialKey KeyRight) Down _ _ -> Just MoveRight
+        EventKey (SpecialKey KeyUp) Down _ _  -> Just MoveUp
+        EventKey (SpecialKey KeyDown) Down _ _ -> Just MoveDown
+        otherwise -> Nothing
---
+++ 0005 Tetris/src/Game.hs
@@ -0,0 +1,126 @@
+module Game where
+
+import Data.List(transpose)
+import System.Random(randomRIO)
+
+data Position = Position Int Int deriving (Eq, Show)
+data BlockColor = Red
+                | Green
+                | Blue
+                | Yellow
+                | Cyan
+                | Orange
+                | Magenta
+                deriving (Bounded, Enum, Eq, Show)
+
+data Cell = Cell BlockColor Position deriving (Eq,Show)
+data Block = Block BlockColor [Position] deriving Show
+data Board = Board [Cell]
+data State = NewBlock
+           | NewBlockImpossible
+           | BlockFalling Position Block
+           deriving Show
+data Level = Level {
+                    lBoard :: Board,
+                    lState :: State,
+                    lScore :: Int
+                  }
+
+data LevelEvent = MoveLeft | MoveRight | MoveDown | MoveUp
+
+initLevel :: Level
+initLevel = Level emptyBoard NewBlock 0
+  where
+    emptyBoard = Board []
+
+startPos :: Position
+startPos = Position (gridWidth `div` 2) (gridHeight - 1)
+
+timeStepHandler :: Float -> Level -> IO Level
+timeStepHandler _ts lvl@(Level board@(Board cells) state score) =
+  case state of
+    NewBlockImpossible -> return lvl
+    NewBlock -> do
+      newBlock <- randomBlock
+      return $ maybe (lvlState NewBlockImpossible) blockFalling $ validate board startPos newBlock
+    BlockFalling p@(Position x y) bl->
+      return $ maybe (mergeblock p bl) blockFalling $ validate board (Position x (y-1)) bl
+    where
+      lvlState x = lvl {lState = x}
+      blockFalling (position,block) = lvlState $ BlockFalling position block
+      mergeblock p b= lvl {lBoard = Board $ removeFullRows $ cells ++ blockToCells p b, lState = NewBlock}
+
+removeFullRows :: [Cell] -> [Cell]
+removeFullRows cells = remapRows $ map selectIfNotFullRow [0..gridHeight-1]
+  where
+    selectRow r = filter (isCellInRow r) cells
+    isCellInRow r (Cell _ (Position _x y)) = r == y
+    selectIfNotFullRow r =
+      let sr = selectRow r in
+      if length sr == gridWidth
+        then []
+        else sr
+    remapRows cells = concat $ zipWith (\i cls -> map (remapCell i) cls) [0..] $ filter (not . null) cells
+    remapCell i (Cell color (Position x _))= Cell color (Position x i)
+
+
+
+
+
+eventHandler :: LevelEvent -> Level -> IO Level
+eventHandler evt lvl@(Level board (BlockFalling pos@(Position x y) block) score) =
+    return $ maybe lvl blockFalling $ validate board p b
+    where
+      (p,b) = case evt of
+        MoveLeft  -> (Position (x-1) y   , block)
+        MoveRight -> (Position (x+1) y   , block)
+        MoveDown  -> (Position x    (y-1), block)
+        MoveUp    -> (Position x     y   , rotateBlock block)
+      lvlState x = lvl {lState = x}
+      blockFalling (position,block) = lvlState $ BlockFalling position block
+      rotateBlock (Block color positions) = Block color $ map rotatePosition positions
+      rotatePosition (Position x y) = Position y (-x)
+eventHandler _ level = return level
+
+validate :: Board -> Position -> Block -> Maybe (Position,Block)
+validate (Board cells) pos@(Position dx dy) bl@(Block color positions) =
+  if any invalidPosition movedPositions
+    then Nothing
+    else Just (pos,bl)
+  where
+    movedPositions = map addPos positions
+    addPos (Position x y) = Position (x+dx) (y+dy)
+    invalidPosition p@(Position x y) = x < 0 || x > gridWidth - 1 || y < 0 || p `elem` cellPositions
+    cellPositions = map cellPosition cells
+    cellPosition (Cell _c p) = p
+
+gridWidth :: Int
+gridWidth = 10
+
+gridHeight :: Int
+gridHeight = 20
+
+blockToCells :: Position -> Block -> [Cell]
+blockToCells (Position dx dy) (Block clr positions) =
+  map (Cell clr . movePos) positions
+  where
+    movePos (Position x y) = Position (x+dx) (y+dy)
+
+randomBlock :: IO Block
+randomBlock = do
+  index <- randomRIO (0, length availableBlocks - 1)
+  return $ availableBlocks !! index
+  where
+    availableBlocks = zipWith Block [minBound..] blockPositions
+    blockPositions = map parseBlock blocks
+    parseBlock = map tupleToPosition . filter notEmpty . list2dTo3Tuple
+    list2dTo3Tuple = concat . zipWith (zip3 [0..] . repeat) [0..]
+    tupleToPosition (x,y,_c) = Position (x - 1) (y-1)
+    notEmpty (_x,_y,'█') = True
+    notEmpty _ = False
+    blocks = transpose . map words $
+              [ "_█ ███ ██ ██ ██ ██_ _██"
+              , "_█ _█_ █_ _█ ██ _██ ██"
+              , "_█ _█_ █_ _█"
+              , "_█"
+              ]
